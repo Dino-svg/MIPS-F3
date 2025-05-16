7ccbe2ffc099d1a3f9e1867cb0e88c1a1f930c05
Dino-svg <angel.cortes8356@alumnos.udg.mx>
2025-05-16 12:04:09 -0600

    Update JAC.v

--- JAC.v
+++ JAC.v
@@ -5,6 +5,6 @@ module JumpAddressCalculator(
     input [25:0] target_address,
     output [31:0] jump_address
 );
-    // Concatenar los 4 bits superiores de PC+4 con los 26 bits de la instrucción (shift left 2)
+    // Concatenar los 4 bits superiores 
     assign jump_address = {PC_plus_4[31:28], target_address, 2'b00};
 endmodule
